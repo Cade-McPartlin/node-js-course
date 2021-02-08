7d30d28a87b0d6d1d24a4b6cbaca144fee7a8d88
Cade McPartlin <cademcp@gmail.com>
2021-02-07 23:50:28 -0600

    Continue section 6. Refactor weather app to use ES6 Object Destructuring and Shorthand.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,24 +2,24 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 // Get location name from command line argument. Ignore the first two arguments.
-const locationName = process.argv.slice(2)[0];
+const address = process.argv[2];
 
 // Only geocode location if a location was provided.
-if (!locationName) {
+if (!address) {
     console.log('Please provide a location');
 } else {
     // Call geocode function to get location information.
-    geocode(locationName, (error, locationData) => {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return console.log(error);
         }
         // Call forecast function to get weather information for specified location using data returned from geocode callback.
-        forecast(locationData.latitude, locationData.longitude, (error, weatherData) => {
+        forecast(latitude, longitude, (error, weatherData) => {
             if (error) {
                 return console.log(error);
             }
 
-            console.log(locationData.location);
+            console.log(location);
             console.log(weatherData);
         });
     });
