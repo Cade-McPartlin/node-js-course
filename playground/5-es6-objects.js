8d4038573ff02d5ed21de0c90d0eb62920c6e109
Cade McPartlin <cademcp@gmail.com>
2021-02-07 23:34:37 -0600

    Continue section 6. First look at Object property shorthand and Object destructuring.

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,54 @@
+//
+// Object property shorthand
+//
+
+const name = 'Cade';
+const userAge = 23;
+
+// Before ES6
+// const user = {
+//     name: name,
+//     age: userAge,
+//     location: 'Brookfield'
+// }
+
+// Shorthand - if the variable name is the same name as the key, the key does not need to be defined (name in this case).
+const user = {
+    name,
+    age: userAge,
+    location: 'Brookfield'
+};
+
+console.log(user);
+
+//
+// Object destructuring -- extract object properties into individual variables.
+//
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+};
+
+// Old
+// const label = product.label;
+// const stock = product.stock;
+
+// New in ES6
+// label:productLabel is creating a new const with name productLabel that holds the value of the label property on the product object.
+// rating = 5 is setting the default value of the rating const ONLY if there is not rating property on the product object.
+const {label:productLabel, stock, rating = 5} = product;
+console.log(productLabel);
+console.log(stock);
+// This will be undefined since it does not exist in the product object.
+console.log(rating);
+
+
+// Destructuring the object inline the function parameters
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock);
+};
+
+transaction('order', product);
