dd23660d4c765dc5af02e16f3dc54121ba19a591
Cade McPartlin <cademcp@gmail.com>
2021-02-14 13:56:17 -0600

    Continue Section 10: Introduction to Promises.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,35 +16,35 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     const db = client.db(databaseName);
 
-    db.collection('users').findOne({ name: 'Jen', age: 1 }, (error, userDocument) => {
-         if (error) {
-             return console.log('Unable to fetch user.');
-         }
-
-         console.log(userDocument);
-    });
-
-    db.collection('users').find({ age: 27 }).toArray((error, userDocuments) => {
-        console.log(userDocuments);
-    });
-
-    db.collection('users').find({ age: 27 }).count((error, userDocuments) => {
-        console.log(userDocuments);
-    });
-
-    db.collection('tasks').findOne({ _id: new ObjectID("60287189bee8c47c1a3c86fd") }, (error, task) => {
-        if (error) {
-            return console.log('Unable to fetch task.');
-        }
-
-        console.log(task);
-    });
-
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        if (error) {
-            return console.log('Unable to fetch tasks.');
-        }
-
-        console.log(tasks);
-    });
+    // db.collection('users').findOne({ name: 'Jen', age: 1 }, (error, userDocument) => {
+    //      if (error) {
+    //          return console.log('Unable to fetch user.');
+    //      }
+    //
+    //      console.log(userDocument);
+    // });
+    //
+    // db.collection('users').find({ age: 27 }).toArray((error, userDocuments) => {
+    //     console.log(userDocuments);
+    // });
+    //
+    // db.collection('users').find({ age: 27 }).count((error, userDocuments) => {
+    //     console.log(userDocuments);
+    // });
+    //
+    // db.collection('tasks').findOne({ _id: new ObjectID("60287189bee8c47c1a3c86fd") }, (error, task) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch task.');
+    //     }
+    //
+    //     console.log(task);
+    // });
+    //
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch tasks.');
+    //     }
+    //
+    //     console.log(tasks);
+    // });
 });
