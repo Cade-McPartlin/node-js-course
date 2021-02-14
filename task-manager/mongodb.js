7191bfa94bb972daeda080942087f357e4b25a23
Cade McPartlin <cademcp@gmail.com>
2021-02-14 14:19:45 -0600

    Continue Section 10: Update operations in Mongodb.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,35 +16,24 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     const db = client.db(databaseName);
 
-    // db.collection('users').findOne({ name: 'Jen', age: 1 }, (error, userDocument) => {
-    //      if (error) {
-    //          return console.log('Unable to fetch user.');
-    //      }
-    //
-    //      console.log(userDocument);
-    // });
-    //
-    // db.collection('users').find({ age: 27 }).toArray((error, userDocuments) => {
-    //     console.log(userDocuments);
-    // });
-    //
-    // db.collection('users').find({ age: 27 }).count((error, userDocuments) => {
-    //     console.log(userDocuments);
-    // });
-    //
-    // db.collection('tasks').findOne({ _id: new ObjectID("60287189bee8c47c1a3c86fd") }, (error, task) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch task.');
+    // db.collection('users').updateOne({ _id: new ObjectID("60286cc5a4b68b7ba7c02b96") }, {
+    //     $inc: {
+    //         age: 1
     //     }
-    //
-    //     console.log(task);
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
     // });
-    //
-    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch tasks.');
-    //     }
-    //
-    //     console.log(tasks);
+
+    // db.collection('tasks').updateMany({ completed: false }, {
+    //    $set: {
+    //        completed: true
+    //    }
+    // }).then((result) => {
+    //     console.log(result.modifiedCount);
+    // }).catch((error) => {
+    //     console.log(error);
     // });
+
 });
