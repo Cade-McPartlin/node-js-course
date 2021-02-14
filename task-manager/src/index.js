65cf967bd511a38b9816f6a166040c9ee3f3b84f
Cade McPartlin <cademcp@gmail.com>
2021-02-14 17:45:24 -0600

    Continue Section 11: Create endpoint for task creation.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 // Ensure mongoose file runs to create db connection.
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +18,16 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.post('/tasks', (req, res) => {
+   const task = new Task(req.body);
+
+   task.save().then(() => {
+       res.send(task);
+   }).catch((e) => {
+       res.status(400).send(e);
+   });
 });
 
 app.listen(port, () => {
