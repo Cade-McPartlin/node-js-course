2a8b64f0af67e55b561ab312225de44148fbc7a5
Cade McPartlin <cademcp@gmail.com>
2021-02-14 17:49:39 -0600

    Continue Section 11: Add status codes for good responses.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,21 +10,23 @@ const port = process.env.PORT || 3000;
 // Automatically parse json into an object.
 app.use(express.json());
 
+// Endpoint to save new users to the MongoDB database.
 app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
 });
 
+// Endpoint to save new tasks to the MongoDB database.
 app.post('/tasks', (req, res) => {
    const task = new Task(req.body);
 
    task.save().then(() => {
-       res.send(task);
+       res.status(201).send(task);
    }).catch((e) => {
        res.status(400).send(e);
    });
