ef4057e0e55db4389d27a6f2f3ba325002410b71
Cade McPartlin <cademcp@gmail.com>
2021-02-14 16:26:40 -0600

    Continue Section 11: Add validation and sanitization to mongoose models.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 // Connect to MongoDB with mongoose. Making sure indexes will be created.
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
@@ -9,30 +10,64 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // Create new user model with Mongoose.
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password: {
+      type: String,
+      required: true,
+        minlength: 7,
+      trim: true,
+      validate(value) {
+          if (value.toLowerCase().includes('password')) {
+              throw new Error('Password cannot contain "password"');
+          }
+      }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
     }
 });
 
 // Create Task model.
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
-// Create new instance of User model.
+// // Create new instance of User model.
 // const me = new User({
-//    name: 'Cade',
-//    age: 'Mike'
+//     name: '    Cade    ',
+//     email: 'MYEMAIL@TEST.COM     ',
+//     password: '123'
 // });
-
-// Save instance of me to MongoDB, returns promise.
+//
+// // Save instance of me to MongoDB, returns promise.
 // me.save().then(() => {
 //     console.log(me);
 // }).catch((error) => {
@@ -40,13 +75,13 @@ const Task = mongoose.model('Task', {
 // });
 
 // Create instance of Task model.
-const cleanHouse = new Task({
-    description: 'Clean the house',
-    completed: false
-});
-
-cleanHouse.save().then(() => {
-    console.log(cleanHouse);
-}).catch((error) => {
-   console.log(error);
-});
+// const cleanHouse = new Task({
+//     description: '          Clean the house',
+//     completed: false
+// });
+//
+// cleanHouse.save().then(() => {
+//     console.log(cleanHouse);
+// }).catch((error) => {
+//    console.log(error);
+// });
