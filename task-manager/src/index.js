b4174b0df36c1ecac57c92f755b0906f89549eb6
Cade McPartlin <cademcp@gmail.com>
2021-02-15 22:54:17 -0600

    Continue Section 11: Add endpoints for fetching all Users, user by id, all tasks, task by id.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,6 +21,31 @@ app.post('/users', (req, res) => {
     });
 });
 
+// Endpoint to get all users.
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((e) => {
+        res.status(500).send(e);
+    });
+});
+
+// Endpoint to get user by id.
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+       if (!user) {
+           return res.status(404).send();
+       }
+
+       res.send(user);
+
+    }).catch((e) => {
+       res.status(500).send(e);
+    });
+});
+
 // Endpoint to save new tasks to the MongoDB database.
 app.post('/tasks', (req, res) => {
    const task = new Task(req.body);
@@ -30,6 +55,31 @@ app.post('/tasks', (req, res) => {
    }).catch((e) => {
        res.status(400).send(e);
    });
+});
+
+// Endpoint to get all tasks.
+app.get('/tasks', (req, res) => {
+   Task.find({}).then((tasks) => {
+       res.send(tasks);
+   }).catch((e) => {
+      res.status(500).send();
+   });
+});
+
+// Endpoint to get task by id.
+app.get('/tasks/:id', (req, res) => {
+   const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+       if (!task) {
+           return res.status(404).send();
+       }
+
+       res.send(task);
+
+    }).catch((e) => {
+        res.status(500).send();
+    });
 });
 
 app.listen(port, () => {
