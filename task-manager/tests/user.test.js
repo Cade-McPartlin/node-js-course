0387eabb5a0511966457a58365839d29621dc187
Cade McPartlin <cademcp@gmail.com>
2021-03-05 21:13:23 -0600

    Section 16: Finish adding tests for User endpoints.

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -98,3 +98,37 @@ test('Should not delete account for unauthenticated user', async () => {
        .send()
        .expect(401);
 });
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+   await request(app)
+       .patch('/users/me')
+       .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+       .send({
+           name: 'Cade1'
+       })
+       .expect(200);
+
+   const user = await User.findById(userOneId);
+   expect(user.name).toEqual('Cade1');
+});
+
+test('Should not update invalid user fields', async () => {
+   await request(app)
+       .patch('/users/me')
+       .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+       .send({
+           location: 'US'
+       })
+       .expect(400);
+});
